609eb83431d29c85add2830990cd631459b63731
ThomasSundberg <thomassundberg@github.com>
2020-01-18 17:03:20 +0100

    Added JiraToolWindow

---
+++ src/main/java/window/JiraToolWindow.java
@@ -0,0 +1,28 @@
+package window;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class JiraToolWindow implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+
+    @Override
+    public void init(ToolWindow window) {
+
+    }
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        return false;
+    }
+
+    @Override
+    public boolean isDoNotActivateOnStart() {
+        return false;
+    }
+}
