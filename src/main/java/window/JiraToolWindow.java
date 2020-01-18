bd53eeaf63ff0a2091601b33564db26be22deffb
ThomasSundberg <thomassundberg@github.com>
2020-01-18 22:02:16 +0100

    Added toolWindow example

--- src/main/java/window/JiraToolWindow.java
+++ src/main/java/window/JiraToolWindow.java
@@ -3,26 +3,22 @@ package window;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 public class JiraToolWindow implements ToolWindowFactory {
+    private JPanel windowContent;
+    private JButton fetchJira;
+
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
-    }
-
-    @Override
-    public void init(ToolWindow window) {
-
-    }
-
-    @Override
-    public boolean shouldBeAvailable(@NotNull Project project) {
-        return false;
-    }
-
-    @Override
-    public boolean isDoNotActivateOnStart() {
-        return false;
+        JiraWindow myToolWindow = new JiraWindow(toolWindow);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
     }
 }
+
